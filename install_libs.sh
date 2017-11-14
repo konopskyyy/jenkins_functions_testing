cc197d5dd0fde025e4537ed5189902d88ceccc5b
konopskyyy <konopskyyy@gmail.com>
2017-11-14 20:52:49 +0100

    install_libs.sh: delete unused libs

--- install_libs.sh
+++ install_libs.sh
@@ -3,10 +3,6 @@
 virtualenv .
 source ./bin/activate
 
-pip install requests
-pip install robotframework-requests
-pip install robotframework-archivelibrary
-pip install robotframework-jsonlibrary
 pip install robotframework-selenium2library
 
 deactivate
